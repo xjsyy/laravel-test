c6bb331c87c7c7e9ae11bbd6a2e6bb4adc51df0c
yyc <Xjsyy>
2023-04-27 20:30:05 +0800

    first commit

---
+++ app/Models/Chirp.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Chirp extends Model
+{
+    protected $fillable = [
+        'message',
+    ];
+
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
+    use HasFactory;
+    public function user(): BelongsTo
+
+    {
+
+        return $this->belongsTo(User::class);
+    }
+}
